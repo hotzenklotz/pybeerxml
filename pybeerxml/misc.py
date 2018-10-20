a751e0f61ae99ba72c853acece9b7a25f86933f9
Niels Kjøller Hansen <niels.k.h@gmail.com>
2018-10-20 20:39:19 +0200

    Implement misc.amount_is_weight as boolean

--- pybeerxml/misc.py
+++ pybeerxml/misc.py
@@ -3,9 +3,17 @@ class Misc(object):
         self.name = None
         self.type = None
         self.amount = None
-        self.amount_is_weight = None
+        self._amount_is_weight = None
         self.use = None
         self.use_for = None
         self.time = None
         self.notes = None
 
+    @property
+    def amount_is_weight(self):
+        return bool(self._amount_is_weight)
+
+    @amount_is_weight.setter
+    def amount_is_weight(self, value):
+        print(value)
+        self._amount_is_weight = value
