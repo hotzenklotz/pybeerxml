d83239bcb11591f8ad162609d15d6153da1643ca
Tom Herold <tom.herold@scalableminds.com>
2020-09-25 14:16:45 +0200

    fix mypy

--- README.md
+++ README.md
@@ -75,7 +75,7 @@ poetry install
 
 Make sure to Test, Lint, Format, & Type-Check your code before sending a pull request:
 ```
-python -m pytest pybeerxml
+python -m pytest tests
 python -m pylint pybeerxml
 python -m black pybeerxml
 python -m mypy pybeerxml
--- pybeerxml/recipe.py
+++ pybeerxml/recipe.py
@@ -27,15 +27,19 @@ class Recipe(object):
     def abv(self):
         return ((1.05 * (self.og - self.fg)) / self.fg) / 0.79 * 100.0
 
+    @abv.setter
+    def set_abv(self, value):
+        pass
+
     # Gravity degrees plato approximations
     @property
     def og_plato(self):
-        og = self.og or self.calc_og()
+        og = self.og
         return (-463.37) + (668.72 * og) - (205.35 * (og * og))
 
     @property
     def fg_plato(self):
-        fg = self.fg or self.calc_fg()
+        fg = self.fg
         return (-463.37) + (668.72 * fg) - (205.35 * (fg * fg))
 
     @property
@@ -49,6 +53,10 @@ class Recipe(object):
                 _ibu += hop.bitterness(ibu_method, self.og, self.batch_size)
 
         return _ibu
+
+    @ibu.setter
+    def set_ibu(self, value):
+        pass
 
     @property
     def og(self):
@@ -74,6 +82,10 @@ class Recipe(object):
 
         return _og
 
+    @og.setter
+    def set_og(self, value):
+        pass
+
     @property
     def fg(self):
 
@@ -92,32 +104,20 @@ class Recipe(object):
 
         return _fg
 
+    @fg.setter
+    def set_fg(self, value):
+        pass
+
     @property
     def color(self):
         # Formula source: http://brewwiki.com/index.php/Estimating_Color
         mcu = 0.0
-        for f in self.fermentables:
-            if f.amount is not None and f.color is not None:
+        for fermentable in self.fermentables:
+            if fermentable.amount is not None and fermentable.color is not None:
                 # 8.3454 is conversion factor from kg/L to lb/gal
-                mcu += f.amount * f.color * 8.3454 / self.batch_size
+                mcu += fermentable.amount * fermentable.color * 8.3454 / self.batch_size
         return 1.4922 * (mcu ** 0.6859)
 
-    @ibu.setter
-    def ibu(self, value):
-        pass
-
-    @fg.setter
-    def fg(self, value):
-        pass
-
-    @og.setter
-    def og(self, value):
-        pass
-
-    @abv.setter
-    def abv(self, value):
-        pass
-
     @color.setter
-    def color(self, value):
+    def set_color(self, value):
         pass
